e40b954bcfa68ee99dfaa7ee7f204555e9f57df7
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-19 12:12:59 -0700

    0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include<stdio>
+int main(){
+        int n;
+        if (n>0){
+                printf("%d", "is positive")
+        }
+        else if (n==0){
+                printf("%d", "is zero")
+        }
+        else {
+                printf("%d", "is negative")
+        }
+
+
+
+}
