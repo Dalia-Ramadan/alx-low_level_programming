9aa639170cef84776e1d8be21a59acbc730618ef
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-20 16:47:55 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints the size of various types on the computer it is compiled and run on.
+* Return: 0
+*/
+int main(void)
+{
+	printf(sizeof(char),"\n",sizeof(int),"\n",sizeof(long int),"\n",sizeof(long long int),"\n",sizeof(float));
+		return (0);
+}
