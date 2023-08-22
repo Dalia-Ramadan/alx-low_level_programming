1f3bbaa25dd48f7373a48873368403af83444d18
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-22 14:42:18 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - generates random valid passwords
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int a, b;
+
+	srand(time(NULL));
+	a= 0;
+	while (b <= 2645)
+	{
+		a = (rand() % 128);
+		b+= a;
+		printf("%c", a);
+	}
+
+	printf("%c", 2772 - b);
+
+	return (0);
+}
