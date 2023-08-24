cfb40dee61520b101de50cb9b456a106a31e8364
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-23 19:47:16 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a=0, b=0;
+	while (dest[a] != '\0')
+		a++;
+
+	if (; b<n; b++)
+	{
+		src[b] != '\0';
+		dest[a] = src[b];
+		a++;
+	}
+
+	dest[a] = '\0';
+
+	return (dest);
+}
+
