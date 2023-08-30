5d85f7d4ac10a22ae6159dbef5b5a66ae893c659
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-30 10:25:32 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _puts_recursion-prints a string
  * @s: is a string
  */
 void _puts_recursion(char *s)
 {
-	printf("%s \n", s);
+	printf("%s\n", s);
 }
