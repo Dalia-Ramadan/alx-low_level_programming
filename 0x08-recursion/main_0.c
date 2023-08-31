f617e9cda43b3751176c86df2228eb0b249e9812
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 16:14:40 -0700

    0-puts_recursion.c

--- 0x08-recursion/main_0.c
+++ 0x08-recursion/main_0.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	_puts_recursion("%c", "First, solve the problem. Then, write the code\n");
+	_puts_recursion("%s %c", "First, solve the problem. Then, write the code\n");
 	return (0);
 }
