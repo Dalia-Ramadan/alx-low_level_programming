d52d598bc3920e44255909f78fe10ff8ce31f04b
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 16:09:32 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,16 +9,4 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 		printf("%c", '\n');
 	printf("%s", s);
-	_puts_recursion(++s);
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
 }
