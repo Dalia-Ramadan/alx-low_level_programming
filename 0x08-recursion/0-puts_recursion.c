b034fe8cc225a2609ca0ef7816b00b463904a5d4
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 16:00:22 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,3 +11,33 @@ void _puts_recursion(char *s)
 	printf("%s", s);
 	_puts_recursion(++s);
 }
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        _puts_recursion("First, solve the problem. Then, write the code");
+        return (0);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        _puts_recursion("Talk is cheap. Show me the code");
+        return (0);
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        _puts_recursion("");
+        return (0);
+}
