b82dc5281efbfbc92b35537c14545cd134fed40d
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 17:05:51 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion-print a string in reverse.
+ * @s: is a string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		_putchar(s);
+
+		_print_rev_recursion(++s);
+	}
+}	
