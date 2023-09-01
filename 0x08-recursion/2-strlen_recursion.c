a523481eb3e0c26b0053bd243650998e25473e20
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 17:59:41 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * int _strlen_recursion-returns the length of a string.
- * @s: is a string
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
+ * Return: the length of string
  */
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\0')
-	{
-		return (_strlen_recursion(++s));
-	}
+		return (1 + _strlen_recursion(s + 1));
 	return (0);
 }
