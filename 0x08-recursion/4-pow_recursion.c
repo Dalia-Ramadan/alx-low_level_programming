c8e618e2a21c34aade755755e3ac1f9d70ecca27
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-08-31 18:21:25 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,13 +7,17 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (x==0 || y==0)
+	if (x==0)
 	{
 		return (0);
 	}
-	else if (x>0 && y>0)
+	else if (y==0)
 	{
-		return (_pow_recursion(x, y));
+		return (x);
+	}
+	else if (x>0 && y>1)
+	{
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else
 		return (-1);
