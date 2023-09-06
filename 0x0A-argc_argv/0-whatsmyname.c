46a8b9aeacfe56f6cbb5d25e78d625c102edce36
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-06 14:03:02 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,15 @@
-#include"main.h"
-#include<stdio.h>
+#include "main.h"
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+	if (argc >= 0)
+    {
+      printf("%s\n", *argv++);
+    }
+  else
+    {
+      printf("The command had no other arguments.\n");
+    }
 
+  return 0;
+}
