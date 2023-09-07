a3ea5c1d0c508516ff6b3645e2020738b91a6978
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-06 18:31:59 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,13 @@
 #include"main.h"
-#include <stdio.h>
+#include<stdio.h>
 /**
- * argc: the counter
- * argv: the array
+ * main - function to print file name
+ * @argc: count of the arguments supplied to the program and the
+ * @argv: an array of pointers to the strings
+ * Return: Always 0
  */
-int main (int argc, __attribute__ ((unused))char *argv[])
+int main(int argc, __attribute__ ((unused))char *argv[])
 {
-	if (argc>1)
-	{
 	printf("%i\n", argc);
-	}
 	return (0);
 }
