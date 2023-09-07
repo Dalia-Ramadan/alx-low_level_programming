eca81d5bedfc43e12977ba289d82a4d1ad70ff00
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-06 17:50:55 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * @argc: the counter.
- * @argv: is a one-dimensional array of strings.
+ * @argc: the counter of the argument
+ * @argv: the argument
  */
 int main (__attribute__ ((unused))int argc, char *argv[])
 {
