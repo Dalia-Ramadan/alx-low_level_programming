e22ec6b702c200d7cc33fa5b1c860cdf70b6b111
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-06 18:41:48 -0700

    1-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * main - function
+ * @argc: number of arguments passed
+ * Return: Always 0
+ * @argv: arguments passed here
+ */
+int main(int argc, char *argv[])
+{
+	for (int i=0; i >= argc; i++)
+	{
+		printf("%s", *argv);
+	}
+	return (0);
+}
