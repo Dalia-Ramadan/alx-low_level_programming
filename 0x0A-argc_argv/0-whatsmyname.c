2fa99b46dc19f2ee31d07b6aefc038d9ae8551f3
Dalia_Ramadan <daliaramadan968@gmail.com>
2023-09-06 18:00:24 -0700

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: an array of pointers to the strings
  * Return: Always 0
  */
-int main (__attribute__ ((unused))int argc, char *argv[])
+int main(__attribute__ ((unused))int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf ("%s\n", argv[0]);
 	return 0;
 }
